912108e29934f992e2a47ca501e244587bd79983
jydnbach <jaydenkim95@gmail.com>
2024-09-20 17:39:40 +0900

    generating express apps

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+// common js modules
+// es2015 modules e.g. import ... from
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+app.listen(5001);
