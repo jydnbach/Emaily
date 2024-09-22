81525ee417e1677aec40c2239b3ecb0273b9699a
jydnbach <jaydenkim95@gmail.com>
2024-09-22 15:59:03 +0900

    wiring passport

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+passport.use(new GoogleStrategy());
+
 const PORT = process.env.PORT || 5000;
 // environment variable
 app.listen(PORT);
