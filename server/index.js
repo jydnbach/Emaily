6a8c3d935fbe607f4653fe08f15bb67f9f38a18b
jydnbach <jaydenkim95@gmail.com>
2024-09-22 15:57:36 +0900

    passport js and its strategy import statements

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 // common js modules
 // es2015 modules e.g. import ... from
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
 
 const PORT = process.env.PORT || 5000;
 // environment variable
