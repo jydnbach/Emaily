7874437d420c5fa1d6b41222b7c1a76947612319
jydnbach <jaydenkim95@gmail.com>
2024-09-23 14:46:34 +0900

    temp change

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 // common js modules
 // es2015 modules e.g. import ... from
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
 const app = express();
-
-passport.use(new GoogleStrategy());
-
-const PORT = process.env.PORT || 5000;
-// environment variable
-app.listen(PORT);
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+app.listen(5001);
