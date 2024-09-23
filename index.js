95fca920a1d74067cabded9829d05d2b8d6324c4
jydnbach <jaydenkim95@gmail.com>
2024-09-23 16:21:34 +0900

    testing oauth

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 // common js modules
 // es2015 modules e.g. import ... from
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
 
 const PORT = process.env.PORT || 5001;
 
