0a7c6dab3a3f52650f1a3e05d250462f7f5c4ab0
jydnbach <jaydenkim95@gmail.com>
2024-09-23 15:51:30 +0900

    revert

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 // common js modules
 // es2015 modules e.g. import ... from
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+
+passport.use(new GoogleStrategy());
+
+const PORT = process.env.PORT || 5001;
+
 app.listen(5001);
