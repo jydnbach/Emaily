4bab0f64bfd6ce75535b3626cef4d67994cbca14
jydnbach <jaydenkim95@gmail.com>
2024-09-28 09:37:54 +0900

    refactor require statements

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 // common js modules
 // es2015 modules e.g. import ... from
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
 
 const app = express();
 
